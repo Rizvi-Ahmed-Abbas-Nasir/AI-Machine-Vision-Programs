0196013b50b863c8e131b3d565abb18789453867
Rizvi Ahmed Abbas <abbasrizvi200209@gmail.com>
2023-02-23 17:39:20 +0530

    adeeed

--- GUI/custtk.py
+++ GUI/custtk.py
@@ -4,29 +4,35 @@ import tkinter
 import PIL
 import customtkinter as ctk
 from PIL import ImageTk
-from PIL.Image import Image
+import PIL.Image
 from PIL.ImageTk import PhotoImage
 import HandTrackingModule
 
-ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
+ctk.set_appearance_mode("system")  # Modes: system (default), light, dark
 ctk.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 app = ctk.CTk(fg_color="#222222")  # create CTk window like you do with the Tk window
-image = PIL.Image.open("logo2.png")
-icon = ImageTk.PhotoImage(image)
 app.columnconfigure(0, weight=1)
 app.title("AI Machine Vision")
-app.geometry("1000x600")
+app.geometry("1080x600")
 
-notebook = ctk.CTkTabview(app, width=1000, height=580, corner_radius=8 , fg_color="#29292a") # 262628 alternate Color
+notebook = ctk.CTkTabview(app, width=1080, height=580, corner_radius=8 , fg_color="#29292a") # 262628 alternate Color
 notebook.add(" Home ")
 notebook.add(" Desktop Controller ")
 notebook.add(" Tranier ")
 notebook.add(" VirtualKeyboard ")
+notebook.add(" Presentation Controller ")
 notebook.grid(padx=10, pady=10)
 notebook._segmented_button.grid(sticky="W")
 
-button_1 = ctk.CTkButton(notebook.tab(" Home "))
-button_1.pack()
+
+ImageHome = ctk.CTkImage(dark_image=PIL.Image.open("AI IMAGE.png"), size=(400, 300))
+
+button_1 = ctk.CTkButton(notebook.tab(" Home "),
+                         image=ImageHome,
+                         text=" ",
+                         fg_color="#29292a" ,
+                         hover_color="#29292a")
+button_1.place(x=20,y=50)
 
 #
 # def button_function():
